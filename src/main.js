de846cc02c3f6ef0426e9620e13862b42b0b7418
Kangdyu <kangdyu99@gmail.com>
2020-08-27 17:03:11 +0900

    bug fix

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
     let prevScrollHeight = 0;
     let currentScene = 0;
     let isEnterNewScene = false;
+    let acc = 0.1;
+    let delayedYOffset = 0;
+    let rafId;
+    let rafState;
 
     const sceneInfo = [
         {
@@ -224,10 +228,10 @@
         switch (currentScene) {
             case 0:
                 // console.log('0 play');
-                let sequence = Math.round(
-                    calcValues(values.imageSequence, currentYOffset)
-                );
-                objs.context.drawImage(objs.videoImages[sequence], 0, 0);
+                // let sequence = Math.round(
+                //     calcValues(values.imageSequence, currentYOffset)
+                // );
+                // objs.context.drawImage(objs.videoImages[sequence], 0, 0);
                 objs.canvas.style.opacity = calcValues(
                     values.canvas_opacity,
                     currentYOffset
@@ -325,10 +329,10 @@
 
             case 2:
                 // console.log('2 play');
-                let sequence2 = Math.round(
-                    calcValues(values.imageSequence, currentYOffset)
-                );
-                objs.context.drawImage(objs.videoImages[sequence2], 0, 0);
+                // let sequence2 = Math.round(
+                //     calcValues(values.imageSequence, currentYOffset)
+                // );
+                // objs.context.drawImage(objs.videoImages[sequence2], 0, 0);
 
                 if (scrollRatio <= 0.5) {
                     objs.canvas.style.opacity = calcValues(
@@ -619,13 +623,16 @@
             prevScrollHeight += sceneInfo[i].scrollHeight;
         }
 
-        if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+        if (
+            delayedYOffset >
+            prevScrollHeight + sceneInfo[currentScene].scrollHeight
+        ) {
             isEnterNewScene = true;
             currentScene++;
             document.body.setAttribute("id", `show-scene-${currentScene}`);
         }
 
-        if (yOffset < prevScrollHeight) {
+        if (delayedYOffset < prevScrollHeight) {
             // 모바일에서 맨 위에서 스크롤 위로 당겼을 시 마이너스 대비
             if (currentScene === 0) return;
             isEnterNewScene = true;
@@ -636,20 +643,64 @@
         if (!isEnterNewScene) playAnimation();
     }
 
-    window.addEventListener("scroll", () => {
-        yOffset = window.pageYOffset;
-        scrollLoop();
-        checkMenu();
-    });
+    function loop() {
+        delayedYOffset = delayedYOffset + (yOffset - delayedYOffset) * acc;
+
+        if (!isEnterNewScene) {
+            if (currentScene === 0 || currentScene === 2) {
+                const currentYOffset = delayedYOffset - prevScrollHeight;
+                const values = sceneInfo[currentScene].values;
+                const objs = sceneInfo[currentScene].objs;
+
+                let sequence = Math.round(
+                    calcValues(values.imageSequence, currentYOffset)
+                );
+                if (objs.videoImages[sequence]) {
+                    objs.context.drawImage(objs.videoImages[sequence], 0, 0);
+                }
+            }
+        }
+        rafId = requestAnimationFrame(loop);
+
+        if (Math.abs(yOffset - delayedYOffset) < 1) {
+            cancelAnimationFrame(rafId);
+            rafState = false;
+        }
+    }
+
     window.addEventListener("load", () => {
+        document.body.classList.remove("before-load");
         setLayout();
         sceneInfo[0].objs.context.drawImage(
             sceneInfo[0].objs.videoImages[0],
             0,
             0
         );
+        window.addEventListener("scroll", () => {
+            yOffset = window.pageYOffset;
+            scrollLoop();
+            checkMenu();
+
+            if (!rafState) {
+                rafId = requestAnimationFrame(loop);
+                rafState = true;
+            }
+        });
+        window.addEventListener("resize", () => {
+            if (window.innerWidth > 900) {
+                setLayout();
+                sceneInfo[3].values.rectStartY = 0;
+            }
+        });
+        window.addEventListener("orientationchange", () => {
+            setTimeout(setLayout, 500);
+        });
+        document
+            .querySelector(".loading")
+            .addEventListener("transitionend", (e) => {
+                document.body.removeChild(e.currentTarget);
+            });
     });
-    window.addEventListener("resize", setLayout);
 
     setCanvasImages();
 })();
